0a94546c59bd93933a45c4cc6a2dd81f4d4714a0
Steven Inouye <steveninouye@msn.com>
2018-09-10 15:37:03 -1000

    completed Code

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,7 +1,66 @@
 class Code
   attr_reader :pegs
+
+  PEGS = {
+    "R" => :red,
+    "G" => :green,
+    "B" => :blue,
+    "Y" => :yellow,
+    "O" => :orange,
+    "P" => :purple
+  }
+  
+  def initialize (pegs)
+    @pegs = pegs
+  end
+  
+
+  def self.parse (str)
+    pegs = str.split("").map do |ltr|
+      raise "Invalid Input" if !PEGS.has_key?(ltr.upcase)
+      PEGS[ltr.upcase]
+    end
+
+    Code.new(pegs)
+  end
+  
+  def self.random
+    pegs = []
+    4.times { pegs << PEGS.values.sample }
+    Code.new(pegs)
+  end
+  
+#############
+
+  def [] (i)
+    pegs[i]
+  end
+
+  def exact_matches(code)
+    matches = @pegs.map.with_index{|el,idx| el if el == code[idx]}.compact
+    matches.count
+  end
+
+  def near_matches(code)
+    colors = (@pegs + code.pegs).uniq
+    num = colors.reduce(0) {|a,c| a + [code.pegs.count(c), @pegs.count(c)].min}
+    num - exact_matches(code)
+  end
+
+  def ==(code)
+    return true if exact_matches(code) == 4
+    false
+  end
+  
 end
 
 class Game
   attr_reader :secret_code
+
+  def initialize
+    
+  end
+
+  def random
+  end
 end
